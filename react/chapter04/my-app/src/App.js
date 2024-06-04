51f540849c24a2cbe6fa6f944a2b192898cb655b
devgiants75 <devgiants75@naver.com>
2024-06-04 14:29:14 +0900

    Update App.js

--- react/chapter04/my-app/src/App.js
+++ react/chapter04/my-app/src/App.js
@@ -9,10 +9,29 @@
 */
 import { FaStar } from "react-icons/fa";
 
+/* 
+  점수에 따라 선택된 별은 빨간색으로 칠해져야 하고, 선택되지 못한 별은 회색으로 표시되야 한다.
+  선택된 속성에 따라 자동으로 별을 만들어내는 컨포넌트를 하나 만들자.
+*/
+/* 
+  10점 별점 시스템이 훨씬 더 자세한 정보를 제공할 수 있다.
+*/
+
+const Star=({selected=false})=>{
+  return <FaStar color={selected?"red":"grey"} />
+}
+
+// 인자로 전달된 갯수 만큼 배열을 생성하는 함수
+const createArray=length=>[...Array(length)]
+
+function StarRating({totalStars=5}){
+  return createArray(totalStars).map((n,i)=><Star key={i} />)
+}
+
 function App() {
   return (
     <div className="App">
-      <FaStar />
+      <StarRating />
     </div>
   );
 }
