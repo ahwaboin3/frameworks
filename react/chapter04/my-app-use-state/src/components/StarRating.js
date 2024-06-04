8d5cc6e7ec572c394580fc0c23872a1e611a36db
devgiants75 <devgiants75@naver.com>
2024-06-04 15:42:58 +0900

    3564gb635b

---
+++ react/chapter04/my-app-use-state/src/components/StarRating.js
@@ -0,0 +1,13 @@
+// src/components/StarRating.js
+import {useState} from "react";
+
+const createArray=length=>[...Array(length)]
+
+export default function StarRating({totalStars=5}){
+    const [selectedStars]=useState(3);
+    return <>
+      {createArray(totalStars).map((n,i)=>
+      <Star key={i} selected={selectedStars>i} />)}
+      <p>{selectedStars} of {totalStars} stars</p>
+    </>
+}
