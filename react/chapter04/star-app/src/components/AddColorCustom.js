ad460cb4847706f853d8b2f0ea5096c8021bc7fb
devgiants75 <devgiants75@naver.com>
2024-06-10 10:45:22 +0900

    24524v5524v

--- react/chapter04/star-app/src/components/AddColorCustom.js
+++ react/chapter04/star-app/src/components/AddColorCustom.js
@@ -7,7 +7,35 @@
     제어가 되는 폼 컴포넌트를 만들 때 필요한 세부 사항을 커스텀 훅으로 묶을 수 있다. 제어가 되는 폼 입력을 만들 때 필요한 중복을 추상화해 없애 주는 우리만의 useInput 훅을 만들어 보자.
 */
 import { useInput } from "./hooks/hooks";
-
+/* 
+    커스텀 훅으로부터 속성을 전개해서 넣는 편이 코드를 복사해 넣는 것보다 훨씬 좋은 코드다.
+    우리가 만든 훅을 사용하면 구현 세부 사항에 신경을 쓰지 않으면서 제어가 되는 폼 입력을 만들 수 있다.
+*/
 export default function AddColorCustom({onNewColor=f=>f}){
     const [titleProps,resetTitle]=useInput("");
+    const [colorProps,resetColor]=useInput("#000000")
+
+    const sumit=event=>{
+        event.preventDefault()
+        onNewColor(titleProps.value,colorProps.value)
+        resetTitle();
+        resetColor();
+    }
+
+    return (
+        <form onSubmit={sumit}>
+            <input
+                {...titleProps}
+                type="text"
+                placeholder="color title..."
+                required
+            />
+            <input
+                {...colorProps}
+                type="color"
+                required
+            />
+            <button>ADD</button>
+        </form>
+    )
 }
