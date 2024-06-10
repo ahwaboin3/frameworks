b2f8041fdae1aa5f3cfd738683c54a50c6db9c12
devgiants75 <devgiants75@naver.com>
2024-06-10 15:45:43 +0900

    Update ColorWithContext.js

--- react/chapter04/star-app/src/components/ColorWithContext.js
+++ react/chapter04/star-app/src/components/ColorWithContext.js
@@ -1,9 +1,17 @@
 // components/ColorWithContext.js.js
+import { FaTrash } from "react-icons/fa";
+import StarRating from "./StarRating";
 
 export default function ColorWithContext({id,title,color,rating}){
+    const {removeColor}=useColors()
     return (
         <section>
             <h1>{title}</h1>
+            <button onClick={()=>removeColor(id)}>
+                <FaTrash />
+            </button>
+            <div style={{height:50,backgroundColor:color}}></div>
+            <StarRating />
         </section>
     )
 
