92e11261ae92999e9fefe40fc54497312e27afa6
devgiants75 <devgiants75@naver.com>
2024-06-11 15:23:51 +0900

    35g64435g6

---
+++ react/chapter05/hooks/src/App2.js
@@ -0,0 +1,9 @@
+// src/App2.js
+
+import WordCount from "./components/WordCount"
+
+function App2(){
+    return <WordCount>You are not going to believe this but...</WordCount>
+}
+
+export default App2
