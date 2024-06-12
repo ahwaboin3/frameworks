491570121373ab250e4ee97e4d803d425508dacb
devgiants75 <devgiants75@naver.com>
2024-06-12 11:47:48 +0900

    sdfsdf

--- react/chapter05/hooks/src/App6.js
+++ react/chapter05/hooks/src/App6.js
@@ -2,13 +2,14 @@
 
 import {useState} from "react"
 import Cat3 from "./components/Cat3"
+import PureCat3 from "./components/Cat3"
 export default function App6(){
     const [cats,setCats]=useState(["Biscuit","Jungle","Outlaw"])
     return (
         <>
             {
                 cats.map((name,i)=>
-                    <Cat3 
+                    <PureCat3 
                         key={i} 
                         name={name}
                         meow={name=>console.log(`${name} has meowed`)}
