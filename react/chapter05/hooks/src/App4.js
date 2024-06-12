31d36353c2dce1f18c62cfcfa087ac2624e6e4ed
devgiants75 <devgiants75@naver.com>
2024-06-12 10:11:52 +0900

    b45y453b654

---
+++ react/chapter05/hooks/src/App4.js
@@ -0,0 +1,7 @@
+//src/App4.js
+
+import Checkbox2 from "./components/Checkbox2";
+
+export default function App4(){
+    return <Checkbox2 />
+}
---
+++ react/chapter05/hooks/src/components/User.js
@@ -0,0 +1,28 @@
+//components/User.js
+/* useReducer로 복잡한 상태 처리하기
+useReducer를 사용하면 상태가 더 복잡해질 때 상태 갱신을 더 예측가능하게 처리하는데 도움이 된다.
+*/
+
+const firstUser={
+    id:0,
+    firstName:"Bill",
+    lastName:"Wilson",
+    city:"Missoula",
+    email:"bwilson@tnwilsons.com",
+    admin:false
+}
+/* firstUser를 초기 상태로 설정한 User라는 컴포넌가 있다. 이 컴포넌트는 적절한 데이터를 표시해준다. */
+import { useState } from "react"
+export default function User(){
+    const[user,setUser]=useState(firstUser)
+    return(
+        <div>
+            <h1>
+                {user.firstName} {user.lastName}-{user.admin?"Admin":"User"}
+            </h1>
+            <p>Email:{user.email}</p>
+            <p>Location:{user.city},{user.state}</p>
+            <button>Make Admin</button>
+        </div>
+    )
+}
