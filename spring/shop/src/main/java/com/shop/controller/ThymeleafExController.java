de498f7d7e2532bd32ce27f1172a0f88502a2abe
devgiants75 <devgiants75@naver.com>
2024-06-14 12:30:42 +0900

    453b45b7457bh

--- spring/shop/src/main/java/com/shop/controller/ThymeleafExController.java
+++ spring/shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -5,12 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 //@Component - @Controller @Repository @Service
 @Controller
 @RequestMapping(value="/thymeleaf")
 public class ThymeleafExController {
 	@GetMapping("/ex01")
 	public String thymeleafExample01(Model model) {
+		model.addAttribute("data","타임리프 예제 입니다.");
+		System.out.println("devtools test");
 		return "tlEx01";
+	}
+	
+	@GetMapping("/ex02")
+	public String thymeleafExample02(Model model) {
+		return "tl/tlEx02";
 	}
 }
