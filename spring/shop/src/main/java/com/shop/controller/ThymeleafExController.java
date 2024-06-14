33968c3105e04be688b1ea5805c84524025d5fa8
devgiants75 <devgiants75@naver.com>
2024-06-14 12:38:29 +0900

    345b645b6745b67

--- spring/shop/src/main/java/com/shop/controller/ThymeleafExController.java
+++ spring/shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,9 +1,13 @@
 package com.shop.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shop.itemdto.ItemDto;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,12 +18,18 @@ public class ThymeleafExController {
 	@GetMapping("/ex01")
 	public String thymeleafExample01(Model model) {
 		model.addAttribute("data","타임리프 예제 입니다.");
-		System.out.println("devtools test");
 		return "tlEx01";
 	}
 	
 	@GetMapping("/ex02")
 	public String thymeleafExample02(Model model) {
+		ItemDto itemDto=new ItemDto();
+		itemDto.setItemDetail("상품 상세 설명");
+		itemDto.setItemNm("테스트 상품1");
+		itemDto.setPrice(10000);
+		itemDto.setRegTime(LocalDateTime.now());
+		
+		model.addAttribute("itemDto",itemDto);
 		return "tl/tlEx02";
 	}
 }
--- spring/shop/src/main/java/com/shop/itemdto/ItemDto.java
+++ spring/shop/src/main/java/com/shop/itemdto/ItemDto.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class ItemDto {
 	private Long id;
 	private String itemNm;
