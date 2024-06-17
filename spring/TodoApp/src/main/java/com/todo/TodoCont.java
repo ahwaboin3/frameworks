976dffc70b8f125fecb17b8d3da609916b7fd7a2
devgiants75 <devgiants75@naver.com>
2024-06-17 12:43:16 +0900

    34t345

--- spring/TodoApp/src/main/java/com/todo/TodoCont.java
+++ spring/TodoApp/src/main/java/com/todo/TodoCont.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,6 +25,14 @@ public class TodoCont {
 //		model.addAttribute("todos",todos);
 		model.addAttribute("todos",tService.getTodos());
 		return "todos";
+	}
+	
+	@PostMapping("/put")
+//	public String putTodo(String content) {
+	public String putTodo(TodoEntity tEntity) {
+//		System.out.println(content);
+		System.out.println(tEntity);
+		return "redirect:/";
 	}
 	
 }
