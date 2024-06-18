e40adb2e8c9300d78b6c4baac5611c3b4c51d6db
devgiants75 <devgiants75@naver.com>
2024-06-18 11:58:49 +0900

    6nurhr67

---
+++ spring/cardatabase/src/main/java/com/cardatabase/cardatabase/domain/Car.java
@@ -0,0 +1,22 @@
+package com.cardatabase.cardatabase.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Car {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String brand, model, color, registerNumber;
+	private int year, price;
+
+}
