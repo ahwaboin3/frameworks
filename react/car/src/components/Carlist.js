5771fcb236ca02304fd3617daa1064494bcda0a5
devgiants75 <devgiants75@naver.com>
2024-06-19 16:00:15 +0900

    Update Carlist.js

--- react/car/src/components/Carlist.js
+++ react/car/src/components/Carlist.js
@@ -1,6 +1,16 @@
 //component/Carlist.js
 import {useState,useEffect} from "react"
 import { SERVER_URL } from "./constants"
+import {DataGrid} from "@mui/x-data-grid"
+
+//ag-grid 컴포넌트는 데이터 표의 열도 정의해야 한다. 여기에는 field는 자동차 객체의 속성이다. 열의 제목은 headerName속성으로 설정 할 수 있다. 열의 너비도 여기에서 설정 가능 하다.
+const columns=[
+    {filed:"brand",headerName:"Brand",width:200},
+    {filed:"model",headerName:"Model",width:200},
+    {filed:"color",headerName:"Color",width:200},
+    {filed:"year",headerName:"Year",width:150},
+    {filed:"price",headerName:"Price",width:150},
+]
 
 function Carlist(){
     //REST API에서 가져온 자동차 정보를 담을 상태 객체가 필요하다
@@ -20,9 +30,16 @@ function Carlist(){
             )
             .catch(err=>console.log(err))
     },[])
-
+    /* DataGrid
+        데이터 표의 데이터 원본은 fetch()로 읽어 들인 cars상태이며, 이는 rows 프롭으로 정의된다.
+        데이터 표 컴포넌트를 이용하려면 모든 행에 getRowId 프롭으로 고유한 ID속성을 정의해야 한다.
+    */
     return(
         <div>
+            <DataGrid 
+                columns={columns} 
+                rows={cars}
+            />
             <table>
                 <tbody>
                     {cars.map((car,index)=>
