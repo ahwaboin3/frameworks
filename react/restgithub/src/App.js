0945df1b9609f97b9825e9805e5cbf7e23651e4c
devgiants75 <devgiants75@naver.com>
2024-06-19 14:42:02 +0900

    Update App.js

--- react/restgithub/src/App.js
+++ react/restgithub/src/App.js
@@ -3,11 +3,18 @@ import {useState} from "react"
 
 function App() {
   const [keyword,setKeyword]=useState("")
+  const [data,setData]=useState([])
+  
   const fetchData=()=>{
     // console.log(keyword)
     fetch(`https://api.github.com/search/repositories?q=${keyword}`)
       .then(response=>response.json())
-      .then(data=>console.log(data))
+      .then(result=>{
+          setData(result.items)
+          // console.log(result.items)
+        }
+      )
+      .catch(err=>console.error(err))
   }
   return (
     <div className="App">
@@ -16,6 +23,20 @@ function App() {
         onChange={e=>setKeyword(e.target.value)}
       />
       <button onClick={fetchData}>Fetch</button>
+      <table>
+        <tbody>
+          {
+            data.map(repo=>
+              <tr>
+                <td>{repo.full_name}</td>
+                <td>
+                  <a href={repo.html_url}>{repo.html_url}</a>
+                </td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
