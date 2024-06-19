167c07cf477592fd2a68b51fea5b8b3e6f8e03c6
devgiants75 <devgiants75@naver.com>
2024-06-19 16:37:46 +0900

    Update Carlist.js

--- react/car/src/components/Carlist.js
+++ react/car/src/components/Carlist.js
@@ -5,11 +5,11 @@ import {DataGrid} from "@mui/x-data-grid"
 
 //ag-grid 컴포넌트는 데이터 표의 열도 정의해야 한다. 여기에는 field는 자동차 객체의 속성이다. 열의 제목은 headerName속성으로 설정 할 수 있다. 열의 너비도 여기에서 설정 가능 하다.
 const columns=[
-    {filed:"brand",headerName:"Brand",width:200},
-    {filed:"model",headerName:"Model",width:200},
-    {filed:"color",headerName:"Color",width:200},
-    {filed:"year",headerName:"Year",width:150},
-    {filed:"price",headerName:"Price",width:150},
+    {field:'brand',headerName:'Brand',width:200},
+    {field:'model',headerName:'Model',width:200},
+    {field:'color',headerName:'Color',width:200},
+    {field:'year',headerName:'Year',width:150},
+    {field:'price',headerName:'Price',width:150},
 ]
 
 function Carlist(){
@@ -22,37 +22,37 @@ function Carlist(){
     //두 번째 인수로 비어 있는 배열을 전달하므로 fetch는 첫 번째 렌더링 후에 한 번만 실행된다. JSON응답 데이터에 있는 자동차 데이터는 cars상태에 저장한다
 
     useEffect(()=>{
-        fetch(SERVER_URL+"/api/cars")
-            .then(res=>res.json())
-            .then(data=>
-                // console.log(data._embedded.cars)
-                setCars(data._embedded.cars)
-            )
-            .catch(err=>console.log(err))
+        fetchCars()
     },[])
     /* DataGrid
         데이터 표의 데이터 원본은 fetch()로 읽어 들인 cars상태이며, 이는 rows 프롭으로 정의된다.
         데이터 표 컴포넌트를 이용하려면 모든 행에 getRowId 프롭으로 고유한 ID속성을 정의해야 한다.
     */
+    const onDelClick=(url)=>{
+        // console.log(url)
+        fetch(url,{method:"delete"})
+            //삭제후에 새로운 목록으로 렌더링
+            .then(()=>fetchCars())
+            .catch(err=>console.error(err))
+    }
+        
+    //서버 주소 상수에서 목록을 가져오는 함수
+    const fetchCars=()=>{
+        fetch(SERVER_URL+"/api/cars")
+                .then(res=>res.json())
+                .then(data=>
+                    // console.log(data._embedded.cars)
+                    setCars(data._embedded.cars)
+                )
+                .catch(err=>console.log(err))
+    }
     return(
-        <div>
+        <div style={{width:'100%'}}>
             <DataGrid 
                 columns={columns} 
                 rows={cars}
-            />
-            <table>
-                <tbody>
-                    {cars.map((car,index)=>
-                        <tr key={index}>
-                            <td>{car.brand}</td>
-                            <td>{car.model}</td>
-                            <td>{car.color}</td>
-                            <td>{car.year}</td>
-                            <td>{car.price}</td>
-                        </tr>    
-                    )}
-                </tbody>
-            </table>
+                getRowId={row=>row._links.self.href}
+                />
         </div>
     )
 }
