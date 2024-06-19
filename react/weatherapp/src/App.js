3d03167bcb8f9faa9fe839da1676dc8ac916ca49
devgiants75 <devgiants75@naver.com>
2024-06-19 11:33:59 +0900

    345b6345b6356b

---
+++ react/weatherapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  fetch("https://api.openweathermap.org/data/3.0/onecall?lat=35.15&lon=129.05&exclude=current&appid=f91f941c5a4e99dede2a98ed106b5c7f
+  ")
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
