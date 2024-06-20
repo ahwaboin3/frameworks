b5a5c23b1630bf028a47707cbf382c84cfba50b0
devgiants75 <devgiants75@naver.com>
2024-06-20 09:23:00 +0900

    Update Carlist.js

--- react/car/src/components/Carlist.js
+++ react/car/src/components/Carlist.js
@@ -3,15 +3,6 @@ import {useState,useEffect} from "react"
 import { SERVER_URL } from "./constants"
 import {DataGrid} from "@mui/x-data-grid"
 
-//ag-grid 컴포넌트는 데이터 표의 열도 정의해야 한다. 여기에는 field는 자동차 객체의 속성이다. 열의 제목은 headerName속성으로 설정 할 수 있다. 열의 너비도 여기에서 설정 가능 하다.
-const columns=[
-    {field:'brand',headerName:'Brand',width:200},
-    {field:'model',headerName:'Model',width:200},
-    {field:'color',headerName:'Color',width:200},
-    {field:'year',headerName:'Year',width:150},
-    {field:'price',headerName:'Price',width:150},
-]
-
 function Carlist(){
     //REST API에서 가져온 자동차 정보를 담을 상태 객체가 필요하다
     //비어 있는 배열을 기본값으로 cars라는 상태를 선언한다
@@ -28,6 +19,27 @@ function Carlist(){
         데이터 표의 데이터 원본은 fetch()로 읽어 들인 cars상태이며, 이는 rows 프롭으로 정의된다.
         데이터 표 컴포넌트를 이용하려면 모든 행에 getRowId 프롭으로 고유한 ID속성을 정의해야 한다.
     */
+   //ag-grid 컴포넌트는 데이터 표의 열도 정의해야 한다. 여기에는 field는 자동차 객체의 속성이다. 열의 제목은 headerName속성으로 설정 할 수 있다. 열의 너비도 여기에서 설정 가능 하다.
+    const columns=[
+        {field:'brand',headerName:'Brand',width:200},
+        {field:'model',headerName:'Model',width:200},
+        {field:'color',headerName:'Color',width:200},
+        {field:'year',headerName:'Year',width:150},
+        {field:'price',headerName:'Price',width:150},
+        {
+            field:"_links.self.href",
+            headerName:'',
+            sortable:false,
+            filterable:false,
+            //셀에 더 복잡한 내용을 넣어햐 할 때는 셀의 내용이 렌더링되는 방법을 정의하는 renderCell프롭을 이용하면 된다
+            //renderCell로 button요소를 추가 한다
+            renderCell:(row)=>
+                <button
+                    onClick={()=>onDelClick(row.id)}>
+                    Delete
+                </button>,
+        },
+    ]
     const onDelClick=(url)=>{
         // console.log(url)
         fetch(url,{method:"delete"})
